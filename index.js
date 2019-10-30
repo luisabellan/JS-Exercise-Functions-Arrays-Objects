78aa94ea59fa2a252b04036a6ba12ccec7588cdf
squarerobin <squarerobin.82@gmail.com>
2019-10-30 05:36:53 +0100

    all stretches done

--- index.js
+++ index.js
@@ -363,9 +363,26 @@ const argTimesTwo = (num) => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
+function carMaker(/* code here */odometer) {
   /* code here */
+
+  let obj = {
+    odometer : odometer,
+    distance : 0,
+    drive: function (distance){
+      this.odometer += distance;
+      return this.odometer;
+    }
+  };
+
+  return obj;
+  
+
+
+
 }
+
+
 
 /// ////// END OF CHALLENGE /////////
 /// ////// END OF CHALLENGE /////////
